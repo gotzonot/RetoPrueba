92129ad334702524325bab152c2904283a7ebe29
talde5-euskera <45936166+talde5-euskera@users.noreply.github.com>
2019-02-01 09:30:37 +0100

    Actualizacion 01/02 Vista Perfil, editar Perfil

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -8,7 +8,7 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-md-8 col-md-offset-2">
-                                <form action="#">
+                                <form action="{{ url('/perfil/actualizar/'.session('idlogin'))}}">
                                     <div class="single-job-content">
                                         <div class="area-title text-center">
 		                                    <h2 class="pt-10 pb-10">MI PERFIL</h2><br/>
@@ -16,49 +16,67 @@
                                         <div class="single-job-form">
 
                                         	<div class="single-info pb-14">
-                                                <label for="f_name" class="uppercase pull-left m-0">Nombre/Apellidos</label>
+                                                <label for="nombreapellidos" class="uppercase pull-left m-0">Nombre/Apellidos</label>
                                                 <div class="form-box fix">
-                                                    <input type="text" id="f_name" name="f_name" placeholder="Introduzca su nombre">
+                                                    <input type="text" id="nombreapellidos" name="nombreapellidos" value="{{$perfil->nombreapellidos}}">
+                                                </div>
+                                            </div>
+
+                                            <div class="single-info pb-14">
+                                                <label for="dni" class="uppercase pull-left m-0">DNI</label>
+                                                <div class="form-box fix">
+                                                    <input type="text" id="dni" name="dni" value="{{$perfil->dni}}">
                                                 </div>
                                             </div>
 
                                             <div class="single-info mb-14">
                                                 <label for="email" class="uppercase pull-left m-0">Correo</label>
                                                 <div class="form-box fix">
-                                                    <input type="text" id="email" name="email" placeholder="Introduzca una dirección de correo">
+                                                    <input type="text" id="email" name="email" value="{{$perfil->email}}">
                                                 </div>
                                             </div>   
                                         </div>
                                         
                                         <div class="single-job-form">
                                             <div class="single-info mb-14">
-                                                <label for="address" class="uppercase pull-left m-0">Dirección</label>
+                                                <label for="direccion" class="uppercase pull-left m-0">Dirección</label>
                                                 <div class="form-box fix">
-                                                    <input type="text" id="address" name="address" placeholder="Introduzca su dirección">
+                                                    <input type="text" id="direccion" name="direccion" value="{{$perfil->direccion}}">
                                                 </div>
                                             </div>
                                             <div class="single-info mb-14">
-                                                <label for="phone" class="uppercase pull-left m-0">Teléfono</label>
+                                                <label for="ciudad" class="uppercase pull-left m-0">Ciudad</label>
                                                 <div class="form-box fix">
-                                                    <input type="text" id="phone" name="phone" placeholder="Introduzca su teléfono de contacto">
+                                                    <input type="text" id="ciudad" name="ciudad" value="{{$perfil->ciudad}}">
+                                                </div>
+                                            </div>
+                                            <div class="single-info mb-14">
+                                                <label for="telefono" class="uppercase pull-left m-0">Teléfono</label>
+                                                <div class="form-box fix">
+                                                    <input type="text" id="telefono" name="telefono" value="{{$perfil->telefono}}">
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="single-info mb-14 fix">
-                                                <label for="photo" class="uppercase pull-left m-0">Curriculum Vitae</label>
-                                                <div class="photo-uploader fix pull-left" id="fileupload">
-                                                    <input type="file" id="cv" name="fileupload">
-                                                     <span class="filename">Sube tu Curriculum aquí</span> 
+                                        <!-- PARA QUE SOLO SE LO VISIONE A UN ALUMNO -->
+                                        @if(session('rol') == "alumno")
+                                            <div class="single-info mb-14 fix">
+                                                    <label for="photo" class="uppercase pull-left m-0">Curriculum Vitae</label>
+                                                    <div class="photo-uploader fix pull-left" id="fileupload">
+                                                        <input type="file" id="cv" name="fileupload">
+                                                        <span class="filename">Sube tu Curriculum aquí</span> 
                                                      <span class="action">Buscar</span> 
                                                 </div>
                                             </div>
-                                        <div class="ml-160 mt-42">
-                                        	<a href="#" class="button button-medium-box">Guardar</a>
-                                        </div>
-
+                                        @endif
+                                        <div class="mt-38">
+                                            <input type="submit" class="btn btn-info mb-15 w-25" id="guardar" name="guardar" value="Guardar">
                                         </div>
                                     </div>
                                 </form>
+                                <form action="{{ url('/perfil')}}">
+                                    <input type="submit" onclick="location.href='1.html';href="{{ url('/perfil')}}" class="btn btn-info mb-15 w-25" value="Atras">
+                                </form>
+                                </div>
                             </div>
                         </div>
                     </div>
