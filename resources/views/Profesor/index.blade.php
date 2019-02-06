f2fe81325e54974dba6e0aa66866a2ffcb5176e4
talde5-euskera <45936166+talde5-euskera@users.noreply.github.com>
2019-02-06 09:00:15 +0100

    Actualizacion 06-02 Activar/desactivar profesor

--- resources/views/Profesor/index.blade.php
+++ resources/views/Profesor/index.blade.php
@@ -54,10 +54,20 @@
                         </div>
                         <div class="address col-3 pl-100">
                             <span class="mtb-30 block">{{$profesor->departamento}}</span>
+                            @if($profesor->baja == 0)
+                                <span class="mtb-30 block" style="color:green"> Activo </span>
+                            @else
+                                <span class="mtb-30 block" style="color:red"> No activo </span>
+                            @endif
                         </div>
                         <div class="keyword col-4 pl-20 pt-39">
                             <a href="{{ url('/profesor/show/'.$profesor->id )}}" class="button mr-10">&#9997; Detalles</a>
-                            <a href="{{url('/profesor/desactivar/'.$profesor->id)}}" class="button mr-10">Desactivar</a>
+                            @if($profesor->baja == 0)
+                                <a href="{{url('/profesor/desactivar/'.$profesor->id)}}" class="button mr-10">Desactivar</a>
+                            @else
+                                <a href="{{url('/profesor/activar/'.$profesor->id)}}" class="button mr-10">
+                                Activar</a>
+                            @endif
                             <a href="{{url('/profesor/eliminar/'.$profesor->id)}}" class="button">&#x2717; Eliminar</a>
                         </div>
                     </div>
